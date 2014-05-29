b2bc92dff854c0d01fe7b33a5356d487fd608547
Ian Olson <ian@cubicleninjas.com>
2014-05-29 16:08:22 -0500

    Update to Functions and Styleguide
    Signed-off-by: Ian Olson <ian@cubicleninjas.com>

--- functions.php
+++ functions.php
@@ -16,21 +16,35 @@
 
 /* ENQUEUE SCRIPTS 				MODIFIED: 5-29-2014
 ------------------------------------------------------*/
+function wp_theme_scripts() {
+
+	// Deregister WordPress jQuery
+	if(!is_admin()) {
+		wp_deregister_script('jquery');
+	}
+
+	// Modernizr
+	wp_enqueue_script( 'modernizr-with-respond', get_template_directory_uri() . '/assets/vendor/modernizr/modernizr-2.6.2-respond-1.1.0.min.js', '', '1.0.0', '');
+}
+
+add_action('wp_enqueue_scripts', 'wp_theme_scripts');
 
 
 /* ENQUEUE STYLES 				MODIFIED: 5-29-2014
 ------------------------------------------------------*/
-// Bootstrap
-function wp_bs_theme_styles() {
-	wp_register_style('wp_bootstrap', get_template_directory_uri() . '/assets/bootstrap/stylesheets/bootstrap.css', array(), '3.1.1', 'all');
-	wp_enqueue_style('wp_bootstrap');
-}
+function wp_theme_styles() {
 
-// Custom Styles
-function wp_custom_theme_styles() {
+	// Bootstrap
+	wp_register_style('wp_bootstrap', get_template_directory_uri() . '/assets/vendor/bootstrap/stylesheets/bootstrap.css', array(), '3.1.1', 'all');
+	wp_enqueue_style('wp_bootstrap');
+
+	// Customer Styles
 	wp_register_style('wp_custom', get_template_directory_uri() . '/assets/styles/style.css', array(), '1.0.0', 'all');
 	wp_enqueue_style('wp_custom');
 }
+
+add_action('wp_enqueue_scripts', 'wp_theme_styles');
+
 
 /* REGISTER NAVIGATION			MODIFIED: 5-29-2014
 ------------------------------------------------------*/
--- styleguide.php
+++ styleguide.php
@@ -1,7 +1,15 @@
 <?php /* Template Name: Styleguide */ get_header(); ?>
-<div class="row">
-	<div class="col-xs-12">
-		<h1 class="page-header">Typography</h1>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-xs-12">
+			<h1 class="page-header">Typography</h1>
+			<h1>H1 - Bootstrap Heading <small>Text Inside &lt;small&gt;</small></h1>
+			<h2>H2 - Bootstrap Heading <small>Text Inside &lt;small&gt;</small></h2>
+			<h3>H3 - Bootstrap Heading <small>Text Inside &lt;small&gt;</small></h3>
+			<h4>H4 - Bootstrap Heading <small>Text Inside &lt;small&gt;</small></h4>
+			<h5>H5 - Bootstrap Heading <small>Text Inside &lt;small&gt;</small></h5>
+			<h6>H6 - Bootstrap Heading <small>Text Inside &lt;small&gt;</small></h6>
+		</div>
 	</div>
 </div>
 <?php get_footer(); ?>
